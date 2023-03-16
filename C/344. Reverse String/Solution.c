93f2b9643fad521d9af20925bebb0e0e3959e73a
Marco Marinho <marcoobom@gmail.com>
2023-03-16 10:07:31 +0100

    Changed repo structe.

--- C/344. Reverse String/Solution.c
+++
@@ -1,7 +0,0 @@
-void reverseString(char* s, int sSize){
-    for(int idx = 0; idx < sSize/2; idx++){
-     char buff = s[idx];
-     s[idx] = s[sSize - idx - 1];
-     s[sSize - idx - 1] = buff;
-    }
-}
