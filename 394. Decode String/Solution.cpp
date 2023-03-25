fdf0d99caa9eeab0790789856886089dd571d205
marco-marinho <marco-marinho@users.noreply.github.com>
2023-03-25 19:59:38 +0100

    75

---
+++ 394. Decode String/Solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> multipliers;
+        stack<string> parts;
+        int num = 0;
+        string temp = "";
+        for (int i = 0; i < s.size(); i++){
+            cout << temp << endl;
+            if (s[i] == '[') {
+                parts.push(temp);
+                multipliers.push(num);
+                num = 0;
+                temp = "";
+            }
+            else if (s[i] == ']') {        
+                int mult = multipliers.top();
+                multipliers.pop();
+                string next = "";
+                for (int j = 0; j < mult; j++){
+                    next += temp;
+                }
+
+                string prev = parts.top();
+                parts.pop();
+                temp = prev + next;
+
+            }
+            else if (isdigit(s[i])){
+                num = 10*num + s[i] - '0';
+            }
+            else temp += s[i];
+        }
+        return temp;
+    }
+};
